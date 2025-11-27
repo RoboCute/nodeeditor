99f260823104a51d8df980e8f389ad917931218e
sailing-innocent <sailing-innocent@foxmail.com>
2025-11-27 12:47:54 +0800

    solve

--- xmake.lua
+++ xmake.lua
@@ -1,23 +1,6 @@
-rule("qt.mywidget")
-    add_deps("qt.ui", "qt.moc", "qt._wasm_app", "qt.qrc", "qt.ts")
-
-    on_config(function (target)
-        -- get qt sdk version
-        local qt = target:data("qt")
-        local qt_sdkver = nil
-        if qt.sdkver then
-            import("core.base.semver")
-            qt_sdkver = semver.new(qt.sdkver)
-        end
-        local frameworks = {"QtGui", "QtWidgets", "QtCore", "QtOpenGL"}
-        import("load")(target, {gui = true, frameworks = frameworks})
-    end)
-rule_end()
-
-
 target("qt_node_editor")
-    set_kind("shared")
-    add_rules("qt.mywidget")
+    add_rules("qt.shared")
+    add_frameworks("QtGui", "QtWidgets", "QtCore", "QtOpenGL")
     add_headerfiles("include/QtNodes/internal/*.hpp")
     add_files("include/QtNodes/internal/*.hpp")
     add_files("resources/resources.qrc")
